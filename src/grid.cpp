4f9a578e035ab60d1704be20cfff24c00cbd8ab0
Kushagra1480 <60288916+Kushagra1480@users.noreply.github.com>
2024-06-29 23:48:09 -0700

    Created Grid Header File

---
+++ src/grid.cpp
@@ -0,0 +1,26 @@
+#include "grid.h"
+#include <iostream>
+
+Grid::Grid() {
+    numRows = 20;
+    numCols = 10;
+    cellSize = 30;
+    Initialize();
+}
+
+void Grid::Initialize() {
+    for (int row = 0; row < numRows; row++) {
+        for (int col = 0; col < numCols; col++) {
+            grid[row][col] = 0;
+        }
+    }
+}
+
+void Grid::Print() {
+    for (int row = 0; row < numRows; row++) {
+        for (int col = 0; col < numCols; col++) {
+            printf("%d ", grid[row][col]);
+        }
+        printf("\n");
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,17 @@
 #include <raylib.h>
+#include "grid.h"
 
 int main()
 {
+    Color darkBlue = {44, 44, 127, 255};
     InitWindow(300, 600, "raylib  Tetris");
     SetTargetFPS(60);
+    Grid grid = Grid();
+    grid.Print();
     while(WindowShouldClose() == false) {
         BeginDrawing();
-        
+        ClearBackground(darkBlue);
+
         EndDrawing();
     }
     CloseWindow();
